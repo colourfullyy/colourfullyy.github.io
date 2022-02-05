6c6c9df978f349538b436e626cdf98730447e230
colourfullyy <phiblu900@gmail.com>
2022-02-04 22:19:43 -0500

    initialize repository

---
+++ README.md
@@ -0,0 +1,1 @@
+# colourfullyy.github.io
---
+++ js/main.js
@@ -0,0 +1,10 @@
+document.addEventListener('mousemove', parallax);
+    function parallax(e) {
+        this.querySelectorAll('.rainbow').forEach(layer => {
+            const speed = layer.getAttribute('data_value')
+            const x = (e.clientX * speed) / 300;
+            const y = (e.clientY * speed) / 300;
+
+            layer.style.transform = `translateX(-${x}px) translateY(-${y}px)`
+    })
+}
