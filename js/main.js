5d09a957264a954be58a8a3137b58dcbc08d97aa
colourfullyy <phiblu900@gmail.com>
2022-02-06 20:32:05 -0500

    finishing portfolio

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ document.addEventListener('mousemove', parallax);
             const speed = layer.getAttribute('data_value')
             const x = (e.clientX * speed) / 300;
             const y = (e.clientY * speed) / 300;
-
+            
             layer.style.transform = `translateX(-${x}px) translateY(-${y}px)`
     })
 }
